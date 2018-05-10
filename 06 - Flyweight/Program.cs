3c01049aaa6ed73b8934ca2be45c8b505d739006
Jason Linden <jasonlinden01@gmail.com>
2018-05-10 12:11:25 +0200

    Completed publication project

--- 06 - Flyweight/Program.cs
+++ 06 - Flyweight/Program.cs
@@ -1,9 +1,6 @@
-﻿using JetBrains.dotMemoryUnit;
+﻿using _06___Flyweight.Publication;
+using _06___Flyweight.Publication.FlyweightFactory;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _06___Flyweight
 {
@@ -11,6 +8,95 @@ namespace _06___Flyweight
     {
         static void Main(string[] args)
         {
+            // Keen observers will probably already notice the problem: 
+            // The Author value we’re passing into our tuple creation is always a new Author() 
+            // instance in this example. Even though the string Name property value of the Author is the same in both cases, 
+            // the underlying Author object is different, and therefore, the generated key that is used for comparison 
+            // within the Library.GetPublication() method differs.
+
+            // The solution is to explicitly pass the same instance of Author to both our Book retrieval attempts,
+            // which we do here in Example2():
+            Example1();
+            Example2();
+            Example3();
+        }
+
+        public static void Example1()
+        {
+            var library = new Library();
+            var book = library.GetPublication(
+            Tuple.Create(
+                new Author("Patrick Rothfuss"),
+                "The Name of the Wind",
+                PublicationType.Book
+                )
+            );
+
+            var graphicNovel = library.GetPublication(
+            Tuple.Create(
+                new Author("Julie Doucet"),
+                "My New York Diary",
+                PublicationType.GraphicNovel
+                )
+            );
+
+            // Try retrieving Publication with same key.
+            book = library.GetPublication(
+                Tuple.Create(
+                    new Author("Patrick Rothfuss"),
+                    "The Name of the Wind",
+                    PublicationType.Book
+                    )
+            );
+        }
+
+        public static void Example2()
+        {
+            // Create library.
+            var library = new Library();
+
+            // Create Author instances.
+            var patrickRothfuss = new Author("Patrick Rothfuss");
+            var julieDoucet = new Author("Julie Doucet");
+
+            // Create or retrieve new book.
+            var book = library.GetPublication(
+                Tuple.Create(
+                    patrickRothfuss,
+                    "The Name of the Wind",
+                    PublicationType.Book
+                    )
+                    );
+
+            var graphicNovel = library.GetPublication(
+                Tuple.Create(
+                    julieDoucet,
+                    "My New York Diary",
+                    PublicationType.GraphicNovel
+                    )
+                    );
+
+            // Try retrieving Publication with same key.
+            book = library.GetPublication(
+                Tuple.Create(
+                    patrickRothfuss,
+                    "The Name of the Wind",
+                    PublicationType.Book
+                    )
+                    );
+        }
+
+        public static void Example3()
+        {
+            var library = new Library();
+
+            // Try to retrieve a Publication with an invalid PublicationType.
+            library.GetPublication(
+                Tuple.Create(
+                    new Author("Dante"),
+                    "Divine Comedy",
+                    PublicationType.Epic
+                    ));
         }
     }
 }
---
+++ 06 - Flyweight/Publication/FlyweightFactory/Library.cs
@@ -0,0 +1,68 @@
+﻿using _06___Flyweight.Publication.ConcreteFlyweight;
+using _06___Flyweight.Publication.Flyweight_Base;
+using System;
+using System.Collections.Generic;
+
+namespace _06___Flyweight.Publication.FlyweightFactory
+{
+    public class Library
+    {
+        /// Stores all publication data privately. Should not be publically accessible
+        /// since we want to force access through GetPublication() method.
+        protected Dictionary<Tuple<Author, string, PublicationType>, IPublication> Publications =
+        new Dictionary<Tuple<Author, string, PublicationType>, IPublication>();
+
+        /// Get the count of all publications in library.
+        public int GetPublicationCount => Publications.Count;
+
+        /// Retrieve a Publication by passed key Tuple.
+        /// If an item with matching key exists, retrieve from private Publications property.
+        /// Otherwise, generate a new instance, add to list, and return result.
+        public IPublication GetPublication(Tuple<Author, string, PublicationType> key)
+        {
+            IPublication publication = null;
+            try
+            {
+                if (Publications.ContainsKey(key))
+                {
+                    publication = Publications[key];
+                }
+                else
+                {
+                    switch (key.Item3)
+                    {
+                        case PublicationType.Book:
+                            // Create a new Book (ConcreteFlyweight) example.
+                            publication = new Book(
+                            author: key.Item1,
+                            pageCount: 662,
+                            publisher: new Publisher("DAW Books"),
+                            title: key.Item2
+                            );
+                            break;
+                        case PublicationType.GraphicNovel:
+                            // Create a new GraphicNovel (ConcreteFlyweight) example.
+                            publication = new GraphicNovel(
+                            author: key.Item1,
+                            illustrator: new Illustrator(key.Item1.Name),
+                            publisher: new Publisher("Drawn & Quarterly"),
+                            title: key.Item2
+                            );
+                            break;
+                        default:
+                            throw new ArgumentException($"[PublicationType.{key.Item3}] is not configured. Publication ('{key.Item2}' by {key.Item1.Name}) cannot be created.");
+                    }
+                    // ...
+                    // Add new publication to global list.
+                    Publications.Add(key, publication);
+                }
+            }
+            catch (ArgumentException exception)
+            {
+            }
+
+            // Return publication, whether newly-created or existing record.
+            return publication;
+        }
+    }
+}
